635809005ce0fdf4184741bf7456313ac45c6e42
John Moughtin-Leay <l39596958@student.ua92.ac.uk>
2025-04-03 09:31:22 +0100

    Add files via upload

---
+++ Website_form.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="project.css"> <!--links css-->
+        <!--links bootstrap-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <meta charset="UTF-8">
+        <title>St Alphonsus Primary School</title>
+    </head>
+</html>
